49600b4b203cf2d8818d6c5acf76678a6c2845fa
Endrey Márk <ungazik@gmail.com>
2023-01-24 20:10:49 +0100

    Streamlining, beautifications

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use std::io;
 use std::collections::HashMap;
+use std::io::Write; // brings `flush` into scope. (Credit to https://stackoverflow.com/a/41387232)
 
 type Label = Option<String>;
-type Operation = Vec<String>;
-type Program = HashMap<Label, Operation>;
+type Words = Vec<String>;
+type Program = HashMap<Label, Words>;
 
 fn main() {
     let mut program: Program = HashMap::new();
     let input_stream_channel = io::stdin();
+    let mut output_stream_channel = io::stdout();
     loop {
+        println!("Type labeled program line to be stored in listing, or a direct command!");
+        print!("> ");
+        output_stream_channel.flush().unwrap(); // Credit to https://stackoverflow.com/a/41387232
         let mut line = String::new();
         input_stream_channel.read_line(&mut line).expect("Read error");
         if line.is_empty() {
@@ -17,22 +22,22 @@ fn main() {
         trim_newline(&mut line);
         let mut words = line.split_whitespace().map(|word| word.to_string());
         if let Some(mut label_or_direct) = words.next() {
-            let operation: Vec<String> = words.collect();
+            let operation_words: Words = words.collect();
             if let Some(trailer_symbol) = label_or_direct.pop() {
                 if trailer_symbol == ':' {
                     if label_or_direct == "START" {
-                        program.insert(None, operation);
+                        program.insert(None, operation_words);
                     } else {
-                        program.insert(Some(label_or_direct), operation);
+                        program.insert(Some(label_or_direct), operation_words);
                     }
                 } else {
                     label_or_direct.push(trailer_symbol);
-                    println!("Direct command: {label_or_direct}");
-                    if label_or_direct == "quit" {
-                        break;
-                    }
-                    if label_or_direct == "list" {
-                        listing(&program);
+                    println!("Direct command: `{label_or_direct}`");
+                    match label_or_direct.as_str() { // Credit to https://stackoverflow.com/a/29268076
+                        "quit" => break,
+                        "list" => listing(&program),
+                        other  => println!("The `{other}` direct command  has no implementation yet!")
+
                     }
                 }
             }
