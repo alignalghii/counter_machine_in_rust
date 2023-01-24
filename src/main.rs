b8a34d031ed6b329e7c1f9042646c9d481bde88a
Endrey Márk <ungazik@gmail.com>
2023-01-24 15:54:15 +0100

    Simple infinite readline loop with quitting possibility

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let input_stream_channel = io::stdin();
+    loop {
+        let mut line = String::new();
+        input_stream_channel.read_line(&mut line).expect("Read error");
+        trim_newline(&mut line);
+        if line.is_empty() || line == "quit" {
+            break;
+        }
+    }
+}
+
+fn trim_newline(line: &mut String) {
+    if let Some(last) = line.pop() {
+        if last != '\n' {
+            line.push(last);
+        }
+    }
 }
