ef744f530ac342674663d4d4d03fd2a689a3643b
Endrey Márk <ungazik@gmail.com>
2023-01-24 18:18:38 +0100

    Command line interpretation for labels and some direct commands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 use std::io;
+use std::collections::HashMap;
+
+type Label = Option<String>;
+type Operation = Vec<String>;
+type Program = HashMap<Label, Operation>;
 
 fn main() {
+    let mut program: Program = HashMap::new();
     let input_stream_channel = io::stdin();
     loop {
         let mut line = String::new();
         input_stream_channel.read_line(&mut line).expect("Read error");
-        trim_newline(&mut line);
-        if line.is_empty() || line == "quit" {
+        if line.is_empty() {
             break;
+        }
+        trim_newline(&mut line);
+        let mut words = line.split_whitespace().map(|word| word.to_string());
+        if let Some(mut label_or_direct) = words.next() {
+            let operation: Vec<String> = words.collect();
+            if let Some(trailer_symbol) = label_or_direct.pop() {
+                if trailer_symbol == ':' {
+                    if label_or_direct == "START" {
+                        program.insert(None, operation);
+                    } else {
+                        program.insert(Some(label_or_direct), operation);
+                    }
+                } else {
+                    label_or_direct.push(trailer_symbol);
+                    println!("Direct command: {label_or_direct}");
+                    if label_or_direct == "quit" {
+                        break;
+                    }
+                    if label_or_direct == "list" {
+                        listing(&program);
+                    }
+                }
+            }
+        } else {
+            println!("Empty line skipped");
         }
     }
 }
@@ -18,4 +48,8 @@ fn trim_newline(line: &mut String) {
             line.push(last);
         }
     }
+}
+
+fn listing(program: &Program) {
+    println!("{program:?}");
 }
